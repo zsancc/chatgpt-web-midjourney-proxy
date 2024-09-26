8c1ca5b266f4b809d6ae15379218b77d5aff1f97
Alex Price <153695676+zsancc@users.noreply.github.com>
2024-09-26 21:46:59 +0800

    Update openapi.ts
    修改默认最大tokens为128k

--- src/api/openapi.ts
+++ src/api/openapi.ts
@@ -590,7 +590,7 @@ export const countTokens= async ( dataSources:Chat.Chat[], input:string ,uuid:nu
     return rz ;
 }
 const getModelMax=( model:string )=>{
-    let max=4;
+    let max=12;
     model= model.toLowerCase();
     if( model.indexOf('8k')>-1  ){
         return 8;
